ab7bc6c88cf3037dbc786680c25f884c35e1dca5
Victor <victor.udedibor@azenia.com>
2023-06-25 21:25:13 +0100

    removed backend storage

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,13 +1,13 @@
-provider "aws" {
-  region                  = "${var.aws_region}"
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "default"
-}
+# provider "aws" {
+#   region                  = "${var.aws_region}"
+#   shared_credentials_file = "~/.aws/credentials"
+#   profile                 = "default"
+# }
 
-terraform {
-  backend "s3" {
-    bucket = "bridge-app"
-    region = "us-east-1"
-    key = "bridge/terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "bridge-app"
+#     region = "us-east-1"
+#     key = "bridge/terraform.tfstate"
+#   }
+# }
